fbf1d6b92e68cfcf69bd1e7b2271883e0c474b58
Jonathan Bruenderman <jonbruenderman@hotmail.com>
2022-11-06 21:16:54 -0800

    playing around

--- app/lib/widgets/card_illuvial.dart
+++ app/lib/widgets/card_illuvial.dart
@@ -2,57 +2,76 @@
 // import 'package:flutter/material.dart';
 
 // class CardIlluvial extends StatefulWidget {
-//    final String buttonText;
 //    final Function onPressed;
+//     final String name;
+//     final String class_name;
+//     final String affinity_name;
+//     final Color backgroundColor;
+//     final String image;
 
-//    CardIlluvial ({required this.onPressed, this.buttonText = 'defaultString'})
+//    CardIlluvial (
+//     {required this.onPressed, 
+//       required this.name,
+//       required this.class_name,
+//       required this.affinity_name,
+//       required this.backgroundColor,
+//       required this.image }
+//     )
 
 //    @override
-//    _CustomWidgetState createState() => _CustomWidgetState();
+//    IlluvialCard createState() => IlluvialCard();
 // }
 
 
-// class _CustomWidgetState extends State<CardIlluvial> {
+// class IlluvialCard extends State<CardIlluvial> {
 
 //  @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Home Page'),
-//       ),
-//       body: Center(
-//         child: Container(
-//           margin: const EdgeInsets.all(30.0),
-//           padding: const EdgeInsets.all(3.0),
-//           height: 150,
-//           decoration: BoxDecoration(
-//               border: Border.all(color: Colors.black),
-//               borderRadius: BorderRadius.circular(10)),
-//           alignment: Alignment.center,
-//           child: Column(
-//             children: [
-//               Container(
-//                 padding: const EdgeInsets.all(20.0),
-//                 child: const Text(
-//                   'You have pushed the button this many times:',
-//                 ),
-//               ),
-//               Container(
-//                 padding: const EdgeInsets.all(10.0),
-//                 child: const Text('test'
-//                 ),
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: (() {
-          
-//         }),
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
+//   Widget build(BuildContext item) {
+//     return Container(
+//             margin: const EdgeInsets.all(5.0),
+//             padding: const EdgeInsets.all(5.0),
+//             width: 17,
+//             decoration: BoxDecoration(
+//                 color: item.backgroundColor,
+//                 borderRadius: BorderRadius.circular(20)),
+//             child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Container(
+//                         padding: const EdgeInsets.all(2.0),
+//                         child: Text(
+//                           item.name,
+//                           style: const TextStyle(
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                       Container(
+//                         padding: const EdgeInsets.all(2.0),
+//                         child: Text(
+//                           item.affinity_name,
+//                           textAlign: TextAlign.right,
+//                           style: const TextStyle(
+//                               color: Colors.white, fontSize: 12),
+//                         ),
+//                       ),
+//                       Container(
+//                         padding: const EdgeInsets.all(2.0),
+//                         child: Text(
+//                           item.class_name,
+//                           textAlign: TextAlign.left,
+//                           style: const TextStyle(
+//                               color: Colors.white, fontSize: 12),
+//                         ),
+//                       )
+//                     ],
+//                   ),
+//                   Container(
+//                       child: Image.asset(item.image, width: 75, height: 75)),
+//                 ]));
 //   }
 // }
